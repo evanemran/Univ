02aa477e99e94a5ff73ef1e3f1508a60d714d5a8
Emran Khandaker Evan <evanemran007@gmail.com>
2021-11-06 04:04:11 +0600

    Error Message & Dark mode bug fixed

--- app/src/main/java/com/evanemran/univ/MainActivity.java
+++ app/src/main/java/com/evanemran/univ/MainActivity.java
@@ -5,14 +5,27 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
+import com.github.ybq.android.spinkit.style.Wave;
 
 import java.util.List;
 
@@ -27,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements WebClickListener{
     RequestManager manager;
     ProgressDialog dialog;
     CardView recycler_holder;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,21 +56,31 @@ public class MainActivity extends AppCompatActivity implements WebClickListener{
         dialog = new ProgressDialog(this);
         manager = new RequestManager(MainActivity.this);
 
-        dialog.setTitle("Connecting...");
-        dialog.show();
+        progressBar = (ProgressBar)findViewById(R.id.loader);
+        Sprite doubleBounce = new Wave();
+        progressBar.setIndeterminateDrawable(doubleBounce);
+
         manager.getUniv(listener, "", "");
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                name = editText_name.getText().toString();
-                country = editText_country.getText().toString();
+                name = editText_name.getText().toString().trim();
+                country = editText_country.getText().toString().trim();
                 if (name.equals("") && country.equals("")){
-                    Toast.makeText(MainActivity.this, "Enter University name or country name.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Enter University name or Country name.", Toast.LENGTH_LONG).show();
                 }else{
-                    dialog.setTitle("Fetching Universities...");
-                    dialog.show();
-                    manager.getUniv(listener, name, country);
+                    recyclerView.setVisibility(View.GONE);
+                    progressBar.setVisibility(View.VISIBLE);
+                    if (name.equals("")){
+                        manager.getUnivByCountry(listener, country);
+                    }
+                    else if (country.equals("")){
+                        manager.getUnivByName(listener, name);
+                    }
+                    else{
+                        manager.getUniv(listener, name, country);
+                    }
                 }
 
             }
@@ -65,10 +90,25 @@ public class MainActivity extends AppCompatActivity implements WebClickListener{
     private final OnFetchDataListener listener = new OnFetchDataListener() {
         @Override
         public void onResponse(List<APIResponse> responses, String message) {
-
-            dialog.dismiss();
-            if (responses==null){
-                Toast.makeText(MainActivity.this, "No Data Found!", Toast.LENGTH_SHORT).show();
+            recyclerView.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.GONE);
+            if (responses==null || responses.isEmpty()){
+//                Toast.makeText(MainActivity.this, "No Data Found!", Toast.LENGTH_SHORT).show();
+                final Dialog alertDialog = new Dialog(MainActivity.this);
+                alertDialog.setContentView(R.layout.custom_alert_dialog);
+                alertDialog.setTitle("Oops!");
+                Button ok_button= (Button) alertDialog.findViewById(R.id.button_alert);
+                alertDialog.show();
+                ok_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+                Window window = alertDialog.getWindow();
+                window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+                window.setGravity(Gravity.CENTER);
+                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 return;
             }
             showResult(responses);
@@ -76,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements WebClickListener{
 
         @Override
         public void onError(String message) {
-            dialog.dismiss();
+            progressBar.setVisibility(View.GONE);
             Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
         }
     };
@@ -86,7 +126,7 @@ public class MainActivity extends AppCompatActivity implements WebClickListener{
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new UnivAdapter(this, responses, this);
         recyclerView.setAdapter(adapter);
-        recycler_holder.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
     }
 
 
